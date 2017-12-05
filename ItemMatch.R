31e8a6a0afb4746eb18d7591cd4a113a4827e710
rtse999 <rtse999@gmail.com>
2017-12-05 23:30:39 +1100

    23:30 - Tuesday 5 December 2017

--- ItemMatch.R
+++ ItemMatch.R
@@ -1,9 +1,23 @@
-library(stringr)
+#
+# Identifying and matching iPhone model data using regex
+#
+# Location: /Users/raymondtse/Dropbox/Analysis/Text Mining/Search Item List/ItemMatch.r
+# First created: Friday 1 December 2017
+# Last modified: 23:17 - Tuesday 5 December 2017
+#
 
+### System time ###
+format(Sys.time(), "%a %b %d %H:%M:%S %Y")
+
+#install.packages("hexbin")
+library(stringr) 
+
+devtools::session_info()
+
+# Search item
 list <- c("iPhone8 - Silver - 64 Gb", "iPhone 7 - 32Gb - Black", "iPhone 6 - Silver - 64Gb")
 list <- str_to_lower(list)
 
-target <- c("iPhone Silver 7 64Gb")
 target <- c("iPhone Silver 7 64 Gb")
 
 target <- str_split(target, boundary("word"))[[1]]
